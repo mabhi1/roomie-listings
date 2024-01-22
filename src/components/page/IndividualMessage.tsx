5c0e68100fa88a54c46743f7cbc3c49308ad1626
Abhishek Mishra <mabhishek0221@gmail.com>
2024-01-22 06:43:58 -0500

    added button icons

--- src/components/page/IndividualMessage.tsx
+++ src/components/page/IndividualMessage.tsx
@@ -1,11 +1,15 @@
 import { Message } from "@/lib/types";
 import { AccordionContent, AccordionTrigger } from "@/components/ui/accordion";
 import resend from "@/resend/resend";
+import Link from "next/link";
 
 export default async function IndividualMessage({ message }: { message: Message }) {
   const { data } = await resend.emails.get(message.messageId);
   if (!data) return <></>;
-  const ad = data.html?.split("<!-- -->")[7].split("</p>")[0];
+  const ad = data.html?.split("<!-- -->")[7].split("</p>")[0].split("/")!;
+  const adType = ad[1];
+  const adId = ad[2].split('"')[0];
+  const adText = ad[2].split(">")[1].split("<")[0];
   const messageText = data.html?.split('<p style="margin-top:1.25rem;margin-bottom:1.25rem">')[1].split("</p>")[0];
   return (
     <>
@@ -17,7 +21,9 @@ export default async function IndividualMessage({ message }: { message: Message 
             {new Date(data.created_at).toLocaleDateString("en-us", { year: "numeric", month: "short", day: "numeric" })}
           </div>
           <div>Ad link</div>
-          <div dangerouslySetInnerHTML={{ __html: ad! }} className="hover:underline hover:underline-offset-2"></div>
+          <Link href={`/${adType}/${adId}`} className="hover:underline hover:underline-offset-2">
+            {adText}
+          </Link>
         </div>
       </AccordionContent>
     </>
