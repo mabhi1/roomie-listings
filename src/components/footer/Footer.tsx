1e8b13655138087e20c62c38a1085963957d8df2
Abhishek Mishra <mabhishek0221@gmail.com>
2024-01-27 18:52:36 -0500

    added terms and conditions in sign up

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="w-full bg-secondary p-3 text-center">
-      Copyright &copy;2024. ROOMIELISTINGS.COM.{" "}
+    <div className="flex w-full flex-col items-center justify-center bg-secondary p-3 md:flex-row md:gap-2">
+      <div>Copyright &copy;2024. ROOMIELISTINGS.COM.</div>
       <Link href="/terms&conditions" className="text-primary underline underline-offset-2">
         Terms and Conditions
       </Link>
